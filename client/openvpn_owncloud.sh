3e68f21fe68bef2bcad5375fc79088ddcd628ea0
alexfmanihuruk <alex@tanihub.com>
2019-03-16 00:27:15 +0700

    add installation snmpd

--- client/openvpn_owncloud.sh
+++ client/openvpn_owncloud.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 echo "get opnvpnClient"
-wget https://raw.githubusercontent.com/alexfmanihuruk/openvpn-install/master/client/owncloud.ovpn
+wget https://raw.githubusercontent.com/alexfmanihuruk/openvpn-install/master/vpnfile/owncloud.ovpn
 
 echo "Connect to vpn server (Observium)"
 openvpn --config  owncloud.ovpn & 
+
+echo "\n"
+echo "\n"
+
+echo "Install snmpd.."
+apt -y install snmpd
+
+echo "Get snmpd.conf"
+wget https://raw.githubusercontent.com/alexfmanihuruk/openvpn-install/master/confd/owncloud/snmpd.conf
+
+mv snmpd.conf /etc/snmp/
+echo "Restart snmpd"
+service snmpd restart
+
--- client/openvpn_wordpres.sh
+++ client/openvpn_wordpres.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 echo "get opnvpnClient"
-wget https://raw.githubusercontent.com/alexfmanihuruk/openvpn-install/master/client/wordpres.ovpn
+wget https://raw.githubusercontent.com/alexfmanihuruk/openvpn-install/master/vpnfile/wordpres.ovpn
 
 echo "Connect to vpn server (Observium)"
 openvpn --config  wordpres.ovpn & 
+
+echo "\n"
+echo "\n"
+
+echo "Install snmpd.."
+apt -y install snmpd
+
+echo "Get snmpd.conf"
+wget https://raw.githubusercontent.com/alexfmanihuruk/openvpn-install/master/confd/wordpres/snmpd.conf
+
+mv snmpd.conf /etc/snmp/
+echo "Restart snmpd"
+service snmpd restart
+
